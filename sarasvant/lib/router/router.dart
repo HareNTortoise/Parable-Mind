47d60fb85de5b4e7d21657de615fd311b159801d
harishsahadev <harishsahadev@gmail.com>
2024-11-19 01:43:32 +0530

    question_banks page done

--- sarasvant/lib/router/router.dart
+++ sarasvant/lib/router/router.dart
@@ -3,16 +3,17 @@ import 'package:go_router/go_router.dart';
 import 'package:sarasvant/apps/teacher/dashboard/dashboard.dart';
 import 'package:sarasvant/apps/teacher/questions/questions.dart';
 import '../apps/teacher/add_question/add_question.dart';
+import '../apps/teacher/question_bank/question_bank.dart';
 
 final GoRouter router = GoRouter(
-  initialLocation: '/teacher-dashboard/questions',
+  initialLocation: '/teacher-dashboard/question-banks',
   routes: [
     GoRoute(path: '/', pageBuilder: (context, state) => MaterialPage(child: Placeholder())),
     GoRoute(
       path: '/teacher-dashboard',
       pageBuilder: (context, state) => MaterialPage(child: TeacherDashboard()),
       routes: [
-        GoRoute(path: '/question-banks', pageBuilder: (context, state) => MaterialPage(child: Placeholder())),
+        GoRoute(path: '/question-banks', pageBuilder: (context, state) => MaterialPage(child: QuestionBankPage())),
         GoRoute(path: '/questions', pageBuilder: (context, state) => MaterialPage(child: Questions())),
       ],
     ),
