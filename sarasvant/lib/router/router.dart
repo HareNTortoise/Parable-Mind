ec44b0aab6dafdbdc003dfc6ae3a106e819ce1f8
harishsahadev <harishsahadev@gmail.com>
2025-03-07 07:24:26 +0530

    routing for question banks nad question page

--- sarasvant/lib/router/router.dart
+++ sarasvant/lib/router/router.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sarasvant/apps/teacher/dashboard/dashboard.dart';
 import '../apps/teacher/add_question/add_question.dart';
+import '../apps/teacher/question_bank/question_bank.dart';
+import '../apps/teacher/questions/questions.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/teacher-dashboard',
@@ -11,7 +13,8 @@ final GoRouter router = GoRouter(
       path: '/teacher-dashboard',
       pageBuilder: (context, state) => MaterialPage(child: TeacherDashboard()),
       routes: [
-        GoRoute(path: '/question-banks', pageBuilder: (context, state) => MaterialPage(child: Placeholder())),
+        GoRoute(path: '/question-banks', pageBuilder: (context, state) => MaterialPage(child: QuestionBankPage())),
+        GoRoute(path: '/questions', pageBuilder: (context, state) => MaterialPage(child: Questions())),
       ],
     ),
     GoRoute(path: '/add-question', pageBuilder: (context, state) => MaterialPage(child: AddQuestion())),
