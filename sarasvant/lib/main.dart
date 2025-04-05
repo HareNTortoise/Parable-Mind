20c7dc43734cbf1322df43f2843568a129d1d176
nailsonseat <aadarshv3@gmail.com>
2025-04-06 00:52:13 +0530

    setup url strat

--- sarasvant/lib/main.dart
+++ sarasvant/lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:sarasvant/router/router.dart';
 import 'apps/auth/blocs/auth_bloc.dart';
+import 'apps/auth/cubit/phone_form/phone_number_form_cubit.dart';
 import 'apps/auth/services/google_auth_services.dart';
 import 'apps/auth/services/phone_auth_services.dart';
+import 'apps/language/blocs/l10n/l10n_bloc.dart';
 import 'constants/app_constants.dart';
+import 'firebase_options.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Sarasvant());
 }
 
@@ -28,6 +38,8 @@ class Sarasvant extends StatelessWidget {
                 googleAuthServices: RepositoryProvider.of<GoogleAuth>(context),
                 phoneAuthServices: RepositoryProvider.of<PhoneAuth>(context)),
           ),
+          BlocProvider(create: (context) => PhoneNumberFormCubit()),
+          BlocProvider(create: (context) => L10nBloc()),
         ],
         child: MaterialApp.router(
           title: AppConstants.appName,
