c95ec24f348ab47ec2187ee25715856327686e6a
nailsonseat <aadarshv3@gmail.com>
2025-04-05 21:34:32 +0530

    update dummy data

--- sarasvant/lib/constants/dummy_data/questions/msq.dart
+++ sarasvant/lib/constants/dummy_data/questions/msq.dart
@@ -1,4 +1,3 @@
-import 'package:sarasvant/models/variable.dart';
 import '../../../models/questions/msq.dart';
 
 final List<MSQ> dummyMSQs = [
@@ -6,7 +5,7 @@ final List<MSQ> dummyMSQs = [
     id: 'msq_1',
     bankId: '201',
     question: 'Which of the following are prime numbers?',
-    variable: [],
+    variableIds: [],
     points: 5,
     options: ['2', '4', '7', '9'],
     answerIndices: [0, 2],
@@ -15,17 +14,7 @@ final List<MSQ> dummyMSQs = [
     id: 'msq_2',
     bankId: '202',
     question: 'Select all the Newton’s Laws of Motion.',
-    variable: [
-      Variable(
-        id: 'var_1',
-        name: 'Force',
-        namePosition: {'start': (0, 0), 'end': (0, 5)},
-        valuePosition: {},
-        unit: '',
-        unitPosition: {},
-        variableType: 'concept',
-      ),
-    ],
+    variableIds: [],
     points: 6,
     options: [
       'Law of Inertia',
@@ -39,7 +28,7 @@ final List<MSQ> dummyMSQs = [
     id: 'msq_3',
     bankId: '203',
     question: 'Which of the following programming languages are statically typed?',
-    variable: [],
+    variableIds: [],
     points: 5,
     options: ['Java', 'Python', 'C++', 'Ruby'],
     answerIndices: [0, 2],
@@ -48,7 +37,7 @@ final List<MSQ> dummyMSQs = [
     id: 'msq_4',
     bankId: '204',
     question: 'Identify the renewable sources of energy.',
-    variable: [],
+    variableIds: [],
     points: 4,
     options: ['Solar', 'Wind', 'Coal', 'Natural Gas'],
     answerIndices: [0, 1],
@@ -57,17 +46,7 @@ final List<MSQ> dummyMSQs = [
     id: 'msq_5',
     bankId: '205',
     question: 'Which of the following are considered acids in chemistry?',
-    variable: [
-      Variable(
-        id: 'var_2',
-        name: 'pH Value',
-        namePosition: {'start': (0, 0), 'end': (0, 8)},
-        valuePosition: {'start': (0, 10), 'end': (0, 12)},
-        unit: '',
-        unitPosition: {},
-        variableType: 'numeric',
-      ),
-    ],
+    variableIds: [],
     points: 6,
     options: ['HCl', 'NaOH', 'CH3COOH', 'KOH'],
     answerIndices: [0, 2],
--- sarasvant/lib/constants/dummy_data/questions/nat.dart
+++ sarasvant/lib/constants/dummy_data/questions/nat.dart
@@ -1,4 +1,3 @@
-import 'package:sarasvant/models/variable.dart';
 import '../../../models/questions/nat.dart';
 
 final List<NAT> dummyNATs = [
@@ -6,69 +5,40 @@ final List<NAT> dummyNATs = [
     id: 'nat_1',
     bankId: '301',
     question: 'What is the value of π (up to 2 decimal places)?',
-    variable: [],
+    variableIds: [],
     points: 5,
-    answer: 314, // Represented as 3.14 * 100 for scaling
+    answer: 3.14,
   ),
   NAT(
     id: 'nat_2',
     bankId: '302',
     question: 'Calculate the acceleration (m/s²) of an object if force is 20 N and mass is 4 kg.',
-    variable: [
-      Variable(
-        id: 'var_1',
-        name: 'Force',
-        namePosition: {'start': (0, 0), 'end': (0, 5)},
-        valuePosition: {'start': (0, 7), 'end': (0, 9)},
-        unit: 'N',
-        unitPosition: {'start': (0, 10), 'end': (0, 11)},
-        variableType: 'numeric',
-      ),
-      Variable(
-        id: 'var_2',
-        name: 'Mass',
-        namePosition: {'start': (1, 0), 'end': (1, 4)},
-        valuePosition: {'start': (1, 6), 'end': (1, 7)},
-        unit: 'kg',
-        unitPosition: {'start': (1, 8), 'end': (1, 10)},
-        variableType: 'numeric',
-      ),
-    ],
+    variableIds: [],
     points: 8,
-    answer: 5, // Answer: 5 m/s²
+    answer: 5.0,
   ),
   NAT(
     id: 'nat_3',
     bankId: '303',
     question: 'What is the boiling point of water at sea level (in Celsius)?',
-    variable: [],
+    variableIds: [],
     points: 3,
-    answer: 100,
+    answer: 100.0,
   ),
   NAT(
     id: 'nat_4',
     bankId: '304',
     question: 'Find the value of 7 × 8.',
-    variable: [],
+    variableIds: [],
     points: 2,
-    answer: 56,
+    answer: 56.0,
   ),
   NAT(
     id: 'nat_5',
     bankId: '305',
     question: 'If the side of a square is 5 cm, calculate its area (in cm²).',
-    variable: [
-      Variable(
-        id: 'var_3',
-        name: 'Side Length',
-        namePosition: {'start': (0, 0), 'end': (0, 11)},
-        valuePosition: {'start': (0, 13), 'end': (0, 14)},
-        unit: 'cm',
-        unitPosition: {'start': (0, 15), 'end': (0, 17)},
-        variableType: 'numeric',
-      ),
-    ],
+    variableIds: [],
     points: 4,
-    answer: 25,
+    answer: 25.0,
   ),
 ];
