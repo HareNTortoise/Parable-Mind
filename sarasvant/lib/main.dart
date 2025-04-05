a3f74ccc087da629696443193496e239c3575ee0
nailsonseat <aadarshv3@gmail.com>
2025-04-06 01:18:05 +0530

    refactor

--- sarasvant/lib/main.dart
+++ sarasvant/lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:sarasvant/router/router.dart';
+import 'apps/auth/blocs/auth_bloc.dart';
+import 'apps/auth/cubit/phone_form/phone_number_form_cubit.dart';
+import 'apps/auth/services/google_auth_services.dart';
+import 'apps/auth/services/phone_auth_services.dart';
+import 'apps/language/blocs/l10n/l10n_bloc.dart';
 import 'constants/app_constants.dart';
+import 'firebase_options.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
