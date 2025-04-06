f364ecada2e2973b807e4ea43f2cbe8306c8b615
nailsonseat <aadarshv3@gmail.com>
2025-04-06 12:21:01 +0530

    testing domain for microservice

--- sarasvant/lib/constants/app_constants.dart
+++ sarasvant/lib/constants/app_constants.dart
@@ -5,4 +5,10 @@ class AppConstants {
   static const String backendDomain = 'https://backend-558499448097.asia-southeast1.run.app/';
 
   static Duration otpTimeOutDuration = const Duration(minutes: 2);
+
+
+
+
+  // Microservice domain, only for testing, microservice should be communicated with backend
+  static const String microserviceDomain = 'http://0.0.0.0:8000';
 }
