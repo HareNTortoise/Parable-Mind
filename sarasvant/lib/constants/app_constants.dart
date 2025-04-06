405bdb6a4b2f983175406621166c95c8893ca773
harishsahadev <harishsahadev@gmail.com>
2025-04-06 10:39:08 +0530

    App name change

--- sarasvant/lib/constants/app_constants.dart
+++ sarasvant/lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String appName = 'Sarasvant';
+  static const String appName = 'Parable Mind';
   static const double desktopScaleWidth = 1980;
   static const double mobileScaleWidth = 450;
   static const String backendDomain = 'https://backend-558499448097.asia-southeast1.run.app/';
