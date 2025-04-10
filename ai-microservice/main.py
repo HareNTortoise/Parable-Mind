d77611c64b3875ef658cbcfff9dab12103089877
nailsonseat <aadarshv3@gmail.com>
2025-04-10 23:54:56 +0530

    change deployment config

--- ai-microservice/main.py
+++ ai-microservice/main.py
@@ -5,6 +5,7 @@ It sets up the application with routes, logging, and custom OpenAPI schema.
 """
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
+import uvicorn
 from app.routes.ping import router as ping_router
 from app.routes.context_generator import router as context_generator_router
 from app.routes.mcq_variation_generator import router as mcq_variation_generator_router
@@ -83,3 +84,6 @@ def custom_openapi():
 
 
 app.openapi = custom_openapi
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8080)
