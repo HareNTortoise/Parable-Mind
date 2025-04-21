911c1caf188a00b610935f496b63fc19deb31e19
nailsonseat <aadarshv3@gmail.com>
2025-04-21 23:32:47 +0530

    added reset event and bulk saver event

--- sarasvant/lib/apps/teacher/blocs/mcq/mcq_bloc.dart
+++ sarasvant/lib/apps/teacher/blocs/mcq/mcq_bloc.dart
@@ -1,13 +1,12 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
-
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import '../../../../models/questions/mcq.dart';
-import '../../../../repositories/mcq.dart';
+import '../../../../repositories/mcq_respository.dart';
 
 part 'mcq_event.dart';
 part 'mcq_state.dart';
-
 
 class MCQBloc extends Bloc<MCQEvent, MCQState> {
   final MCQRepository repository;
@@ -16,8 +15,17 @@ class MCQBloc extends Bloc<MCQEvent, MCQState> {
     on<FetchMCQs>((event, emit) async {
       emit(MCQLoading());
       try {
-        final mCQs = await repository.getMCQs(event.bankId);
-        emit(MCQLoaded(mCQs));
+        final response = await repository.getMCQs(event.bankId);
+
+        if (response.statusCode! >= 400) {
+          throw StateError(response.data['error'] ?? 'Failed to fetch MCQs.');
+        }
+
+        final mcqs = (response.data as List)
+            .map((item) => MCQ.fromJson(item as Map<String, dynamic>))
+            .toList();
+
+        emit(MCQLoaded(mcqs));
       } catch (e) {
         emit(MCQError(e.toString()));
       }
@@ -25,7 +33,12 @@ class MCQBloc extends Bloc<MCQEvent, MCQState> {
 
     on<CreateMCQ>((event, emit) async {
       try {
-        await repository.createMCQ(event.mcq);
+        final response = await repository.createMCQ(event.mcq);
+
+        if (response.statusCode! >= 400) {
+          throw StateError(response.data['error'] ?? 'Failed to create MCQ.');
+        }
+
         add(FetchMCQs(event.mcq.bankId)); // Reload MCQs
       } catch (e) {
         emit(MCQError(e.toString()));
@@ -34,8 +47,13 @@ class MCQBloc extends Bloc<MCQEvent, MCQState> {
 
     on<UpdateMCQ>((event, emit) async {
       try {
-        await repository.updateMCQ(event.id, event.mcq);
-        add(FetchMCQs(event.mcq.bankId)); // Reload MCQs
+        final response = await repository.updateMCQ(event.id, event.mcq);
+
+        if (response.statusCode! >= 400) {
+          throw StateError(response.data['error'] ?? 'Failed to update MCQ.');
+        }
+
+        add(FetchMCQs(event.mcq.bankId));
       } catch (e) {
         emit(MCQError(e.toString()));
       }
@@ -43,8 +61,34 @@ class MCQBloc extends Bloc<MCQEvent, MCQState> {
 
     on<DeleteMCQ>((event, emit) async {
       try {
-        await repository.deleteMCQ(event.id);
-        add(FetchMCQs(event.id)); // Reload MCQs
+        final response = await repository.deleteMCQ(event.id);
+
+        if (response.statusCode! >= 400) {
+          throw StateError(response.data['error'] ?? 'Failed to delete MCQ.');
+        }
+
+        add(FetchMCQs(event.id));
+      } catch (e) {
+        emit(MCQError(e.toString()));
+      }
+    });
+
+    on<SaveBulkMCQs>((event, emit) async {
+      emit(MCQLoading());
+      try {
+        final response = await repository.createBulkMCQs(event.mcqs);
+
+        if (response.statusCode! >= 400) {
+          throw StateError(response.data['error'] ?? 'Failed to save bulk MCQs.');
+        }
+
+        Logger().i('Bulk MCQs saved successfully: ${response.data['message']}');
+
+        final mcqs = (response.data['mcqs'] as List)
+            .map((item) => MCQ.fromJson(item as Map<String, dynamic>))
+            .toList();
+
+        emit(MCQLoaded(mcqs));
       } catch (e) {
         emit(MCQError(e.toString()));
       }
